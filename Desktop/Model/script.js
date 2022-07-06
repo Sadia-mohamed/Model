cf0f80c8eba8bce6c3411a36674eae0a05e0abb0
sadia-mohamed <sacduushm@gmail.com>
2022-07-06 16:29:09 +0300

    initial commit

---
+++ Desktop/Model/script.js
@@ -0,0 +1,40 @@
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+
+console.log(btnCloseModal);
+
+for (let i = 0; i < btnsOpenModal.length; i++){
+    btnsOpenModal[i].addEventListener('click', function(){
+        console.log(btnsOpenModal[i]);
+        modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+    })
+}
+
+
+    btnCloseModal.addEventListener('click', function(){
+        modal.classList.add('hidden');
+        overlay.classList.add('hidden');
+    })
+
+
+    overlay.addEventListener('click', function(){
+        modal.classList.add('hidden');
+        overlay.classList.add('hidden');
+    })
+
+
+    document.addEventListener('keydown' ,function(e){
+        // console.log(e.key);
+    if(e.key === 'Escape'){
+        if(!modal.classList.contains("hidden")){
+            
+            modal.classList.add("hidden");
+            overlay.classList.add("hidden");
+
+        };
+        
+    }    
+    })
